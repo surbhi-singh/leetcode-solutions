ffa70ca40baa63c18bed82c6c7e5dec0ddc7d0a0
Surbhi Singh <sidarora848@gmail.com>
2017-08-25 18:50:42 -0700

    Solution for TwoSum

--- TwoSum.java
+++ TwoSum.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int length = nums.length;
-
         int returnIndex[] = {-1, -1};
         for(int i = 0; i<length-1; i++)
         {
