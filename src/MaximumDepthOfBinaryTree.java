ffba3f959829537367da2c9239dbfee82966349f
Surbhi Singh <sidarora848@gmail.com>
2017-09-15 17:16:07 -0700

    solution for Maximum Depth Of Binary Tree problem

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        else{
+            return Math.max(1+maxDepth(root.left), 1+maxDepth(root.right));
+        }
+
+    }
+}
