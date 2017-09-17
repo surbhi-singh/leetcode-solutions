7f4a58c34757e729bdc814b635fdbb93d46524ac
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-09-16 22:27:47 -0700

    solution for Delete Node In a Linked List problem

---
+++ src/DeleteNodeInALinkedList.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode ptr = node.next;
+        node.val = ptr.val;
+        node.next = ptr.next;
+    }
+}
