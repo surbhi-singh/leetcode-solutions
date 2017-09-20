3b6e5d31fc3c3ee03c4fe2c95aaff67a0926f9c9
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-09-19 20:36:45 -0700

    Solution for Reverse Vowels of a String problem

--- src/ReverseVowelsOfAString.java
+++ src/ReverseVowelsOfAString.java
@@ -3,10 +3,11 @@ class Solution {
         StringBuilder str = new StringBuilder(s);
         StringBuilder strRev = new StringBuilder(s);
         int i=0, j=s.length()-1;
+        String vowels = "aeiouAEIOU";
         System.out.println(i + " "+ j);
         while(i<j){
-            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'                             ||str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O' || str.charAt(i) == 'U'){
-                while(!(strRev.charAt(j) == 'a' || strRev.charAt(j) == 'e' || strRev.charAt(j) == 'i' || strRev.charAt(j) == 'o' ||                                     strRev.charAt(j) == 'u' || strRev.charAt(j) == 'A' || strRev.charAt(j) == 'E' || strRev.charAt(j) == 'I' ||                                     strRev.charAt(j) == 'O' || strRev.charAt(j) == 'U')){
+            if(vowels.contains(str.charAt(i)+"")){
+                while(!vowels.contains(strRev.charAt(j)+"")){
                     j--;
                 }
                 char temp = str.charAt(i);
