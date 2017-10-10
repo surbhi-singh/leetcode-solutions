68e20279a3a80e2e2ca3cbc57a399d4a8143e47d
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-10-10 04:23:37 -0700

    solution for two sum.java

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,7 +1,6 @@
-import java.util.*;
 class Solution {
-    int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i =0; i<nums.length; i++)
         {
             int complement = target - nums[i];
@@ -11,15 +10,5 @@ class Solution {
                 map.put(nums[i], i);
         }
         return new int[] {-1, -1};
-    }
-}
-public class TwoSum{
-    public static void main(String[] args)
-    {
-        Solution solution = new Solution();
-        int arr[] = {2, 4, 1, 12, 5};
-        int target = 13;
-        arr = solution.twoSum(arr, target);
-        System.out.println(arr[0]+" "+arr[1]);
     }
 }
