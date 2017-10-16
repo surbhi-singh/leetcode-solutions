89016596e5eef9a2ba65f52cc3c6688151ba0c9a
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-10-15 18:40:06 -0700

    solution for Merge Sorted Array in java

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,10 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i, j=0, k=0;
-        for(i=m; i<m+n; i++, j++){
-            nums1[i] = nums2[j];
+        int i=m-1, j=n-1, k=m+n-1;
+        for(; i>=0 && j>=0;k--) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
         }
-        Arrays.sort(nums1);
-
+        while(j>=0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
