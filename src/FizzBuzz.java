1e652b27f3f1a324d3e21ebfef964717e46a2c29
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-11-05 19:54:04 -0700

    solution for FizzBuzz in java

---
+++ src/FizzBuzz.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        if(n <= 0) {
+            return result;
+        }
+        else {
+            for(int i=1; i<=n; i++) {
+                if(i%15 == 0) {
+                    result.add("FizzBuzz");
+                }
+                else if(i%5 == 0) {
+                    result.add("Buzz");
+                }
+                else if(i%3 == 0) {
+                    result.add("Fizz");
+                }
+                else {
+                    result.add(i+"");
+                }
+            }
+        }
+        return result;
+    }
+}
