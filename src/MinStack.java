217e58791cf6ae85e2baa8aa28ea5513c7602848
Surbhi Singh <surbhi.chauhan011@gmail.com>
2018-01-08 20:43:02 -0700

    Solution for Min Stack in java

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,35 +1,43 @@
 class MinStack {
 
-    TreeMap<Integer, Integer> map;
-    Stack<Integer> stack;
+    Stack<List> stack;
+    int minValue;
     /** initialize your data structure here. */
     public MinStack() {
-        map = new TreeMap<>();
         stack = new Stack<>();
+        minValue = Integer.MAX_VALUE;
     }
 
     public void push(int x) {
-        stack.push(x);
-        if(map.containsKey(x)) map.put(x, map.get(x)+1);
-        else map.put(x, 1);
+        List temp = new ArrayList<>();
+        temp.add(x);
+        if(minValue <= x) temp.add(minValue);
+        else {
+            temp.add(x);
+            minValue = x;
+        }
+        stack.push(temp);
     }
 
     public void pop() {
-        int tempnum = stack.pop();
-        int count = map.get(tempnum);
-        if(count == 1) map.remove(tempnum);
-        else map.put(tempnum, count-1);
+        stack.pop();
+        try {
+            List temp = stack.peek();
+            minValue = (int)temp.get(1);
+        }
+        catch(Exception ex) {
+            minValue = Integer.MAX_VALUE;
+        }
     }
 
     public int top() {
-        return stack.peek();
+        List temp = stack.peek();
+        return (int)temp.get(0);
     }
 
     public int getMin() {
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            return entry.getKey();
-        }
-        return 0;
+        List temp = stack.peek();
+        return (int)temp.get(1);
     }
 }
 
